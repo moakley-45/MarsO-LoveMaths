67b1d133c030c7ad8e96d5867103ddfd9dcf8b08
Mars Oakley <a.oakley4589@gmail.com>
2024-10-16 11:07:28 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,10 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    } else {
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    } 
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`
     }
@@ -70,7 +73,9 @@ function calculateCorrectAnswer(){
         return [operand1 + operand2, "addition"]
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"]
-    }else {
+    } else if (operator === "-")  {
+        return [operand1 - operand2, "subtract"]
+    } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`
     }
@@ -94,7 +99,7 @@ function incrementWrongAnswer(){
     document.getElementById("incorrect").innerText = ++oldScore;
 }
 
-function displayAdditionQuestion(operand1, operand2){
+function displayAdditionQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
@@ -102,11 +107,15 @@ function displayAdditionQuestion(operand1, operand2){
 
 }
 
-function displaySubtractQuestion(){
+function displaySubtractQuestion(operand1, operand2) {
     
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+
 }
 
-function displayMultiplyQuestion(operand1, operand2){
+function displayMultiplyQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
